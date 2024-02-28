202589a215ed0fcb36f1a8d04723b39011eff55e
Al Ferro Putra Yusanda <79499749+Ferituaku@users.noreply.github.com>
2024-02-28 09:54:43 +0700

    Update PrismaSegitiga.java

--- Praktikum 2/Tugas Tambahan/PrismaSegitiga.java
+++ Praktikum 2/Tugas Tambahan/PrismaSegitiga.java
@@ -1,3 +1,8 @@
+//Nama: Al Ferro Putra Yusanda
+//NIM: 24060122140164
+//Nama File: PrismaSegitiga.java
+//Desc: Fungsi method dan atribut yang menyusun prisma segitiga (hitung volume dan hitung luas permukaan)
+
 public class PrismaSegitiga {
     private Segitiga alas;
     private double tinggiPrisma;
