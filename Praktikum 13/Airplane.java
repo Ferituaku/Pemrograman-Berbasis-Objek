6585ffb3746429e758e7299480ec18c6fa4d5913
Al Ferro Putra Yusanda <79499749+Ferituaku@users.noreply.github.com>
2024-06-02 22:11:27 +0700

    Delete Praktikum 13 directory

--- Praktikum 13/Airplane.java
+++
@@ -1,8 +0,0 @@
-/*Nama      : Al Ferro Putra Yusanda        */
-/*Nim       : 24060122140164                */
-/*Tanggal   : 1 Juni 2024                   */
-/*Nama File : Airplane.java                 */
-
-public abstract class Airplane extends Vehicle implements iFlyer {
-        
-}
