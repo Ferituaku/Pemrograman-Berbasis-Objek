d1b9b1bc9ed10b8d35a2d674448f006d5c065a5f
Al Ferro Putra Yusanda <79499749+Ferituaku@users.noreply.github.com>
2024-06-05 22:28:13 +0700

    Update Mahasiswa.java

--- Praktikum 14/jdbc/model/Mahasiswa.java
+++ Praktikum 14/jdbc/model/Mahasiswa.java
@@ -1,7 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+/*Nama      : Al Ferro Putra Yusanda        */
+/*Nim       : 24060122140164                */
+/*Tanggal   : 4 Juni 2024                   */
+/*Nama File : Model.java                 */
 package jdbc.model;
 
 /**
