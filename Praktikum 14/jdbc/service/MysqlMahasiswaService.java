cf74cab729b346ba7ca7324f0a2beb0d5cc863b6
Al Ferro Putra Yusanda <79499749+Ferituaku@users.noreply.github.com>
2024-06-05 22:29:27 +0700

    Update MysqlMahasiswaService.java

--- Praktikum 14/jdbc/service/MysqlMahasiswaService.java
+++ Praktikum 14/jdbc/service/MysqlMahasiswaService.java
@@ -1,7 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/*Nama      : Al Ferro Putra Yusanda        */
+/*Nim       : 24060122140164                */
+/*Tanggal   : 4 Juni 2024                   */
+/*Nama File : MysqlMahasiswaService.java    */
+
 package jdbc.service;
 import java.sql.*;
 import java.util.*;
